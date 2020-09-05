411131448a192d8f51fc13b20b606d4ff32fadf8
EmersonBraun <emersonfbraun@gmail.com>
2020-09-05 14:53:02 -0300

    feat: create more user seeders

--- database/seeders/_DatabaseSeeder.ts
+++ database/seeders/_DatabaseSeeder.ts
@@ -23,6 +23,7 @@ import {
   ProductAsingmentFactory,
 } from 'Database/factories'
 import { OrderFactory } from 'Database/factories/OrderFactory'
+import User from 'App/Models/User'
 
 export default class AddressSeeder extends BaseSeeder {
   public async run () {
@@ -49,6 +50,34 @@ export default class AddressSeeder extends BaseSeeder {
     await OperationFactory.createMany(10)
     await StockOperationFactory.createMany(10)
     await SynonymFactory.createMany(10)
+    await User.createMany([
+      {
+        name: 'admin of system',
+        username: 'admin',
+        password: 'secret',
+        activity: 'boss',
+        complete_name: 'Muri',
+        email: 'mauri@mauri.com',
+        rg: '123456789',
+        cpf_cnpj: '123.456.789-12',
+        nick: 'admin',
+        is_provider: true,
+        inactive: false,
+      },
+      {
+        name: 'user test',
+        username: 'test',
+        password: 'secret',
+        activity: 'test',
+        complete_name: 'test',
+        email: 'test@test.com',
+        rg: '987654321',
+        cpf_cnpj: '987.654.321-98',
+        nick: 'test',
+        is_provider: true,
+        inactive: false,
+      },
+    ])
 
     // await OrderHasProductsFactory.createMany(10)
     // await CardHasUsersFactory.createMany(10)
